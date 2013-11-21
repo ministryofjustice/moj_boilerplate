0f4677de56ff26bdc68086eda3bb7dd3572c8cce
Dom Smith <dom@teneightfive.com>
2013-11-21 10:01:10 +0000

    Corrected folder path

--- lib/moj_kickstart/engine.rb
+++ lib/moj_kickstart/engine.rb
@@ -1,7 +1,7 @@
 module MojKickstart
   class Engine < ::Rails::Engine
     initializer 'moj_boilerplate.load_moj_boilerplate' do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/source"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
     end
   end
 end
